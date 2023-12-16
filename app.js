73b24a6833b429360023653c62044aaca0cf5373
Janhavi003 <janhavi.hivarekar@kalvium.community>
2023-12-16 17:25:25 +0530

    random image function done

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const search = document.getElementById("submit")
 
-
+async function fetchRandomImage() {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const data = await response.json();
+    const imageUrl = data.meals[0].strMealThumb;
+    return imageUrl;
+   }
+   function updateImage(imageUrl) {
+    const imageElement = document.getElementById('random-image');
+    imageElement.src = imageUrl;
+   }
+   async function handleRefresh() {
+    const imageUrl = await fetchRandomImage();
+    updateImage(imageUrl);
+   }
+   window.onload = handleRefresh;
