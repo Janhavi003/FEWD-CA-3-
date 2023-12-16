4f6e48d5e47298ad2ddf52c5759075da265a7c78
Janhavi003 <janhavi.hivarekar@kalvium.community>
2023-12-16 17:41:42 +0530

    random image with name done

--- app.js
+++ app.js
@@ -2,15 +2,24 @@
 async function fetchRandomImage() {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
     const data = await response.json();
-    const imageUrl = data.meals[0].strMealThumb;
-    return imageUrl;
-   }
-   function updateImage(imageUrl) {
+    const mealData = data.meals[0];
+    const imageUrl = mealData.strMealThumb;
+    const imageName = mealData.strMeal;
+    return { imageUrl, imageName };
+}
+
+function updateImage(imageUrl, imageName) {
     const imageElement = document.getElementById('random-image');
+    const nameElement = document.getElementById('image-name');
     imageElement.src = imageUrl;
-   }
-   async function handleRefresh() {
-    const imageUrl = await fetchRandomImage();
-    updateImage(imageUrl);
-   }
-   window.onload = handleRefresh;
+    nameElement.innerText = imageName;
+}
+
+async function handleRefresh() {
+    const { imageUrl, imageName } = await fetchRandomImage();
+    updateImage(imageUrl, imageName);
+}
+
+window.onload = handleRefresh;
+
+
