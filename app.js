dbd4e5c3edfd2b0b1d3ca31ebf24b9be32715caf
Janhavi003 <janhavi.hivarekar@kalvium.community>
2023-12-18 12:00:50 +0530

    search result images with text done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Random image with image name
 
 async function fetchRandomImage() {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
@@ -22,4 +23,35 @@ async function handleRefresh() {
 
 window.onload = handleRefresh;
 
+async function searchImages() {
+    const searchInput = document.getElementById('search_bar').value;
+    const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchInput}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        displayResults(data.meals);
+    } catch (error) {
+        console.error('Error fetching images:', error);
+    }
+}
+
+function displayResults(meals) {
+    const resultsContainer = document.getElementById('search_results');
+    resultsContainer.innerHTML = '';
+
+    meals.forEach(meal => {
+        const imgElement = document.createElement('img');
+        imgElement.src = meal.strMealThumb;
+
+        const textElement = document.createElement('p');
+        textElement.textContent = meal.strMeal;
+
+        const resultItem = document.createElement('div');
+        resultItem.appendChild(imgElement);
+        resultItem.appendChild(textElement);
+
+        resultsContainer.appendChild(resultItem);
+    });
+}
 
