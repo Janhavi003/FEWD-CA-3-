bc8ca536719c7b6f693835e0a81e86b3b87920bc
Janhavi003 <janhavi.hivarekar@kalvium.community>
2023-12-19 11:59:41 +0530

    modal done

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Random image with image name
-
+let id;
 async function fetchRandomImage() {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
     const data = await response.json();
     const mealData = data.meals[0];
     const imageUrl = mealData.strMealThumb;
     const imageName = mealData.strMeal;
+    id = mealData.idMeal
     return { imageUrl, imageName };
 }
 
@@ -54,4 +55,34 @@ function displayResults(meals) {
         resultsContainer.appendChild(resultItem);
     });
 }
+const closeBtn = document.getElementById("closeBtn")
+closeBtn.addEventListener('click',function(){
+    modal.style.display = "none"
+})
 
+const RandonImg = document.getElementById("random-image")
+const modal = document.getElementById("modal")
+const ingredientsList = document.getElementById("Ingredients")
+
+async function getIngredients(e){
+    ingredientsList.innerHTML = ""
+    try{
+        let response = await fetch (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${e}`)
+        let data  = await response.json();
+        for(let i=1;i<21;i++){
+            if(data.meals[0][`strIngredient${i}`] != ""){
+                let a = data.meals[0][`strIngredient${i}`]
+                console.log(a)
+                ingredientsList.innerHTML += `<li>${a}</li>`
+            }
+        }
+        
+    }
+    catch (error){
+        console.log("Error fetching data:", error)
+    }
+}
+RandonImg.addEventListener('click',function(){
+    getIngredients(id)
+modal.style.display = "block";
+})
